de601633cd6dd832501146e2fa6981308ffba61c
Gethyn Jones <gethyn1@gmail.com>
2019-05-13 11:04:07 +0100

    Create app structure

---
+++ src/app/app.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => (
+  <div>
+    <h1>This is my app!!!</h1>
+  </div>
+)
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
