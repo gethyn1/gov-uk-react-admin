380ddd9a7764fc36801113c6d74327586ece1162
Gethyn Jones <gethyn1@gmail.com>
2019-05-13 11:42:43 +0100

    Add GDS fonts

--- src/app/app.js
+++ src/app/app.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import 'normalize.css'
+import './app.css'
+import Page from '@govuk-react/page'
+import BackLink from '@govuk-react/back-link'
+import { H1 } from '@govuk-react/heading'
 
 const App = () => (
-  <div>
-    <h1>This is my app!!!</h1>
-  </div>
+  <Page beforeChildren={<BackLink href="#" />}>
+    <H1>Page title</H1>
+  </Page>
 )
 
 ReactDOM.render(
