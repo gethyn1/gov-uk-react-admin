4dfc63001a2f208ab613e3d7ee985036b4623464
Gethyn Jones <gethyn1@gmail.com>
2019-05-16 12:35:50 +0100

    Add store to example integration test

--- src/app/state/claims/actions.js
+++ src/app/state/claims/actions.js
@@ -2,6 +2,9 @@ import { CALL_API } from '../../middleware/api-service'
 import * as types from './types'
 
 const getClaims = () => ({
+  // TODO type is added as integreation test throws an error if type is undefined. Needs looking into
+  // TODO refactor to call api function
+  type: CALL_API,
   [CALL_API]: {
     endpoint: '/claims',
     types: types.READ
--- src/test/integration/claims.integration.test.js
+++ src/test/integration/claims.integration.test.js
@@ -1,11 +1,31 @@
 import React from 'react'
 import 'jest-dom/extend-expect'
 import { createMemoryHistory } from 'history'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import { render, cleanup } from 'react-testing-library'
 import { Router } from 'react-router-dom'
+import { rootReducer } from '../../app/state'
 import { App } from '../../app'
+import { Claims } from '../../app/views/pages'
 
 afterEach(cleanup)
+
+// this is a handy function that I normally make available for all my tests
+// that deal with connected components.
+// you can provide initialState or the entire store that the ui is rendered with
+function renderWithRedux (
+  ui,
+  { initialState, store = createStore(rootReducer, initialState) } = {}
+) {
+  return {
+    ...render(<Provider store={store}>{ui}</Provider>),
+    // adding `store` to the returned utilities to allow us
+    // to reference it in our tests (just try to avoid using
+    // this to test implementation details).
+    store
+  }
+}
 
 // this is a handy function that I would utilize for any component
 // that relies on the router being in context
@@ -25,8 +45,29 @@ function renderWithRouter (
   }
 }
 
+function renderApp (ui, route = '/') {
+  const store = createStore(rootReducer, {})
+  const history = createMemoryHistory({ initialEntries: [route] })
+
+  return {
+    ...render(
+      <Provider store={store}>
+        <Router history={history}>
+          {ui}
+        </Router>
+      </Provider>
+    )
+  }
+}
+
 test('full app rendering/navigating', () => {
-  const { getByText } = renderWithRouter(<App />)
+  const { getByText } = renderApp(<App />)
   // normally I'd use a data-testid, but just wanted to show this is also possible
-  expect(getByText('Claims page')).toBeInTheDocument()
+  expect(getByText('Claims')).toBeInTheDocument()
+})
+
+test('Renders claims page', () => {
+  const { getByText } = renderWithRedux(<Claims />)
+  // normally I'd use a data-testid, but just wanted to show this is also possible
+  expect(getByText('Claims')).toBeInTheDocument()
 })
